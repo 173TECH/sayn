aa71ffda5fade061716f6dd59a720ea20bc97d94
robin-173 <robin@173tech.com>
2020-05-28 11:29:25 +0100

    changed logging to print for init

--- sayn/start_project/start_project.py
+++ sayn/start_project/start_project.py
@@ -1,4 +1,3 @@
-import logging
 import os
 from pathlib import Path
 import shutil
@@ -8,14 +7,15 @@ def sayn_init(sayn_project_name):
     src = Path(os.path.dirname(os.path.abspath(__file__)), 'sayn_project_base').absolute()
     dst = Path(sayn_project_name).absolute()
 
-    logging.info('Creating SAYN base project at the following location: {dst}...'.format(dst=dst))
+    print('Creating SAYN base project at the following location: {dst}...'.format(dst=dst))
     try:
         shutil.copytree(src, dst)
 
         # rename the sample_settings.yaml to settings.yaml
         os.rename(Path(dst, 'sample_settings.yaml').absolute(), Path(dst, 'settings.yaml').absolute())
 
-        logging.info('SAYN base project created.')
-        logging.info('For more information about SAYN visit our documentation: https://173tech.github.io/sayn/')
+        print('SAYN base project created.')
+        print('For more information about SAYN visit our documentation:')
+        print('https://173tech.github.io/sayn/')
     except OSError as e:
         print(e)
