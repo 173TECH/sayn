41293d3a48984d43bca7a8a4723a94a88f13d20d
Adrian Macias <56698+iadrich@users.noreply.github.com>
2020-10-12 17:28:38 +0100

    Refactored tutorial

--- sayn/scaffolding/data/init_project/python/load_data.py
+++ sayn/scaffolding/data/init_project/python/load_data.py
@@ -1,47 +1,47 @@
-from .utils.log_creator import prepare_data, generate_load_query
+from .utils.log_creator import get_data, get_create_table
 from sayn import PythonTask
 
 
 class LoadData(PythonTask):
     def setup(self):
-        err = False
-
         # we use this list to control how many battles we want per tournament
-        tournament_battles = [
+        self.tournament_battles = [
             {"tournament_id": 1, "n_battles": 1000},
             {"tournament_id": 2, "n_battles": 250},
             {"tournament_id": 3, "n_battles": 500},
         ]
 
-        try:
-            self.data_to_load = prepare_data(tournament_battles)
-        except Exception as e:
-            err = True
-            self.tracker.error(e)
+        self.set_run_steps(
+            [
+                "Generate Data",
+                "Load fighters",
+                "Load arenas",
+                "Load tournaments",
+                "Load battles",
+            ]
+        )
 
-        if err:
-            return self.fail()
-        else:
-            return self.success()
+        return self.success()
 
     def run(self):
-        # load the logs
-        for log_type, log_details in self.data_to_load.items():
-            # create table
-            self.tracker.info("Creating table: {log_type}.".format(log_type=log_type))
+        self.start_step("Generate Data")
+        data_to_load = get_data(self.tournament_battles)
+        self.finish_current_step()
 
-            self.default_db.execute(log_details["create"])
+        for log_type, log_data in data_to_load.items():
+            self.start_step(f"Load {log_type}")
 
-            # load logs
-            self.tracker.info("Loading logs: {log_type}.".format(log_type=log_type))
-            logs = log_details["data"]
+            # Create table
+            q_create = get_create_table(log_type)
+            result = self.default_db.execute(q_create)
+            if result.is_err:
+                return result
 
-            for log in logs:
-                q_insert = generate_load_query(log_type, log)
+            # Load logs
+            result = self.default_db.load_data(f"logs_{log_type}", None, log_data)
+            if result.is_err:
+                return result
 
-                self.default_db.execute(q_insert)
-
-            # done
-            self.tracker.info("Done: {log_type}.".format(log_type=log_type))
+            self.finish_current_step()
 
         return self.success()
--- sayn/scaffolding/data/init_project/python/utils/log_creator.py
+++ sayn/scaffolding/data/init_project/python/utils/log_creator.py
@@ -1,90 +1,56 @@
 import random
-import time
+from uuid import uuid4
 
 # Table creation queries
 
-q_create_fighters = """
-    DROP TABLE IF EXISTS {user_prefix}logs_fighters
-    ;
-
-    CREATE TABLE {user_prefix}logs_fighters (
-        event_id VARCHAR,
-        fighter_id INTEGER,
-        fighter_name VARCHAR
-    )
-    ;
-"""
-
-q_create_arenas = """
-    DROP TABLE IF EXISTS {user_prefix}logs_arenas
-    ;
-
-    CREATE TABLE {user_prefix}logs_arenas (
-        event_id VARCHAR,
-        arena_id INTEGER,
-        arena_name VARCHAR
-    )
-    ;
-"""
-
-q_create_tournaments = """
-    DROP TABLE IF EXISTS {user_prefix}logs_tournaments
-    ;
-
-    CREATE TABLE {user_prefix}logs_tournaments (
-        event_id VARCHAR,
-        tournament_id INTEGER,
-        tournament_name VARCHAR
-    )
-    ;
-"""
-
-q_create_battles = """
-    DROP TABLE IF EXISTS {user_prefix}logs_battles
-    ;
-
-    CREATE TABLE {user_prefix}logs_battles (
-        event_id VARCHAR,
-        tournament_id INTEGER,
-        battle_id INTEGER,
-        arena_id INTEGER,
-        fighter1_id INTEGER,
-        fighter2_id INTEGER,
-        winner_id INTEGER
-    )
-    ;
-"""
-
-# Log Details
-
-fighter_logs = [
-    (1, "son goku"),
-    (2, "john"),
-    (3, "lucy"),
-    (4, "dr. x"),
-    (5, "carlos"),
-    (6, "el mucho"),
-    (7, "maciassito"),
-    (8, "kayo"),
-    (9, "vatossito"),
-    (10, "reniot"),
-    (11, "pepe"),
-    (12, "anonymous"),
-]
-
-arena_logs = [
-    (1, "earth canyon"),
-    (2, "world edge"),
-    (3, "namek"),
-    (4, "volcanic crater"),
-    (5, "underwater"),
-]
-
-tournament_logs = [
-    (1, "world championships"),
-    (2, "tenka-ichi budokai"),
-    (3, "king of the mountain"),
-]
+log_types = {
+    "fighters": {
+        "columns": {"fighter_id": "INTEGER", "fighter_name": "VARCHAR"},
+        "values": [
+            "Son Goku",
+            "John",
+            "Lucy",
+            "Dr. x",
+            "Carlos",
+            "El mucho",
+            "Dr. y?",
+            "Kayo",
+            "Vatossito",
+            "Reniot",
+            "Reniota",
+            "Anonymous",
+        ],
+    },
+    "arenas": {
+        "columns": {"arena_id": "INTEGER", "arena_name": "VARCHAR"},
+        "values": [
+            "Earth Canyon",
+            "World Edge",
+            "Namek",
+            "Volcanic Crater",
+            "Underwater",
+        ],
+    },
+    "tournaments": {
+        "columns": {"tournament_id": "INTEGER", "tournament_name": "VARCHAR"},
+        "values": [
+            "World Championships",
+            "Tenka-ichi Budokai",
+            "King of the Mountain",
+        ],
+    },
+    "battles": {
+        "columns": {
+            "event_id": "VARCHAR",
+            "tournament_id": "INTEGER",
+            "battle_id": "INTEGER",
+            "arena_id": "INTEGER",
+            "fighter1_id": "INTEGER",
+            "fighter2_id": "INTEGER",
+            "winner_id": "INTEGER",
+        }
+    },
+}
 
 
 def create_battle_logs(tournament_battles):
@@ -94,19 +60,19 @@ def create_battle_logs(tournament_battles):
         tournament_id = tb["tournament_id"]
         n_battles = tb["n_battles"]
 
-        for n in range(n_battles):
-            battle_id = n + 1
+        for battle_id in range(n_battles):
             # determine fighters
-            fighter1_id = random.randint(1, 12)
-            fighter2_id = random.randint(1, 12)
-            # loop to avoid similar fighter
+            fighter1_id = random.randrange(len(log_types["fighters"]["values"]))
+            fighter2_id = random.randrange(len(log_types["fighters"]["values"]))
+            # loop to avoid same fighter
             while fighter1_id == fighter2_id:
-                fighter2_id = random.randint(1, 12)
+                fighter2_id = random.randrange(len(log_types["fighters"]["values"]))
+
             # determine arenas
-            arena_id = random.randint(1, 5)
+            arena_id = random.randrange(len(log_types["arenas"]["values"]))
             # determine winner
-            if fighter1_id == 1 or fighter2_id == 1:
-                winner_id = 1
+            if fighter1_id == 0 or fighter2_id == 0:
+                winner_id = 0
             else:
                 rand = random.uniform(0, 1)
                 if rand <= 0.5:
@@ -114,14 +80,15 @@ def create_battle_logs(tournament_battles):
                 else:
                     winner_id = fighter2_id
 
-            row = (
-                tournament_id,
-                battle_id,
-                arena_id,
-                fighter1_id,
-                fighter2_id,
-                winner_id,
-            )
+            row = {
+                "event_id": str(uuid4()),
+                "tournament_id": tournament_id,
+                "battle_id": battle_id,
+                "arena_id": arena_id,
+                "fighter1_id": fighter1_id,
+                "fighter2_id": fighter2_id,
+                "winner_id": winner_id,
+            }
 
             battle_logs.append(row)
 
@@ -131,24 +98,21 @@ def create_battle_logs(tournament_battles):
 # Data Package
 
 
-def prepare_data(tournament_battles, user_prefix=""):
+def get_data(tournament_battles, user_prefix=""):
     logs = {
-        "fighters": {
-            "create": q_create_fighters.format(user_prefix=user_prefix),
-            "data": fighter_logs,
-        },
-        "arenas": {
-            "create": q_create_arenas.format(user_prefix=user_prefix),
-            "data": arena_logs,
-        },
-        "tournaments": {
-            "create": q_create_tournaments.format(user_prefix=user_prefix),
-            "data": tournament_logs,
-        },
-        "battles": {
-            "create": q_create_battles.format(user_prefix=user_prefix),
-            "data": create_battle_logs(tournament_battles),
-        },
+        "fighters": [
+            {"fighter_id": id, "fighter_name": val}
+            for id, val in enumerate(log_types["fighters"]["values"])
+        ],
+        "arenas": [
+            {"arena_id": id, "arena_name": val}
+            for id, val in enumerate(log_types["arenas"]["values"])
+        ],
+        "tournaments": [
+            {"tournament_id": id, "tournament_name": val}
+            for id, val in enumerate(log_types["tournaments"]["values"])
+        ],
+        "battles": create_battle_logs(tournament_battles),
     }
 
     return logs
@@ -157,54 +121,12 @@ def prepare_data(tournament_battles, user_prefix=""):
 # Log Load
 
 
-def generate_load_query(log_type, log, user_prefix=""):
-    event_id = time.time()
+def get_create_table(log_type, user_prefix=""):
+    q_drop = f"DROP TABLE IF EXISTS {user_prefix}logs_{log_type}"
 
-    if log_type == "fighters":
-        q_insert = """
-            INSERT INTO {user_prefix}logs_fighters VALUES('{event_id}', {fighter_id}, '{fighter_name}')
-        """.format(
-            user_prefix=user_prefix,
-            event_id=event_id,
-            fighter_id=log[0],
-            fighter_name=log[1],
-        )
+    columns_def = ",\n".join(
+        [f"{column} {type}" for column, type in log_types[log_type]["columns"].items()]
+    )
+    q_create = f"CREATE TABLE {user_prefix}logs_{log_type} ({columns_def})"
 
-    elif log_type == "arenas":
-        q_insert = """
-            INSERT INTO {user_prefix}logs_arenas VALUES('{event_id}', {arena_id}, '{arena_name}')
-        """.format(
-            user_prefix=user_prefix,
-            event_id=event_id,
-            arena_id=log[0],
-            arena_name=log[1],
-        )
-
-    elif log_type == "tournaments":
-        q_insert = """
-            INSERT INTO {user_prefix}logs_tournaments VALUES('{event_id}', {tournament_id}, '{tournament_name}')
-        """.format(
-            user_prefix=user_prefix,
-            event_id=event_id,
-            tournament_id=log[0],
-            tournament_name=log[1],
-        )
-
-    elif log_type == "battles":
-        q_insert = """
-            INSERT INTO {user_prefix}logs_battles VALUES('{event_id}', {tournament_id}, {battle_id}, {arena_id}, {fighter1_id}, {fighter2_id}, {winner_id})
-        """.format(
-            user_prefix=user_prefix,
-            event_id=event_id,
-            tournament_id=log[0],
-            battle_id=log[1],
-            arena_id=log[2],
-            fighter1_id=log[3],
-            fighter2_id=log[4],
-            winner_id=log[5],
-        )
-
-    else:
-        pass
-
-    return q_insert
+    return f"{q_drop};\n\n{q_create};"
