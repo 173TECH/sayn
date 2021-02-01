550710feb5a5b4ed85e39fe1a78ebbfe3d660bfa
Adrian Macias <56698+iadrich@users.noreply.github.com>
2021-02-01 10:48:09 +0000

    Fixes cascade and alter index for pg

--- sayn/database/templates/move_table.sql
+++ sayn/database/templates/move_table.sql
@@ -1,7 +1,7 @@
 {% set src_schema = src_schema+'.' if src_schema else '' -%}
 {% set dst_schema = dst_schema+'.' if dst_schema else '' %}
 
-DROP TABLE IF EXISTS {{ dst_schema }}{{ dst_table }};
+DROP TABLE IF EXISTS {{ dst_schema }}{{ dst_table }}{{ ' CASCADE' if needs_cascade else ''}};
 ALTER TABLE {{ src_schema }}{{ src_table }} RENAME TO {{ dst_table }};
 {% if dst_schema != src_schema %}
 ALTER TABLE {{ src_schema }}.{{ dst_table }};
@@ -10,7 +10,7 @@ ALTER TABLE {{ src_schema }}.{{ dst_table }};
 {% if indexes is defined %}
   {% for name, idx_def in indexes.items() %}
     {% if not cannot_alter_indexes %}
-ALTER INDEX {{ dst_schema }}{{ src_table }}_{{ name }} RENAME TO {{ dst_schema }}{{ dst_table }}_{{ name }};
+ALTER INDEX {{ dst_schema }}{{ src_table }}_{{ name }} RENAME TO {{ dst_table }}_{{ name }};
     {% else %}
 DROP INDEX IF EXISTS {{ dst_schema }}{{ src_table }}_{{ name }};
 CREATE INDEX {{ dst_table }}_{{ name }} ON {{ dst_schema }}{{ dst_table }}({{ ', '.join(idx_def['columns']) }});
