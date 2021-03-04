2dbdfe48f3f78e33bf74efd81cdce530289c03e5
iadrich <56698+iadrich@users.noreply.github.com>
2021-03-04 19:07:36 +0000

    Fixes to step counter in task tracker (#122)

--- sayn/logging/task_event_tracker.py
+++ sayn/logging/task_event_tracker.py
@@ -7,7 +7,7 @@ class TaskEventTracker:
     _logger = None
     _task_name = None
     _task_order = None
-    _steps = list()
+    _steps = None
     _current_step = None
     _current_step_start_ts = None
 
@@ -15,6 +15,7 @@ class TaskEventTracker:
         self._logger = logger
         self._task_name = task_name
         self._task_order = task_order
+        self._steps = list()
 
     def _report_event(self, event, **details):
         details["event"] = event
@@ -37,8 +38,15 @@ class TaskEventTracker:
         self._report_event("set_run_steps", steps=steps)
         self._steps = steps
 
+    def add_run_steps(self, steps):
+        self._report_event("set_run_steps", steps=steps)
+        self._steps.extend(steps)
+
     def start_step(self, step):
         self.finish_current_step()
+
+        if step not in self._steps:
+            self._steps.append(step)
 
         self._current_step = step
         self._current_step_start_ts = datetime.now()
