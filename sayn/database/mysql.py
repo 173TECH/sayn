8de1e9deea0839046091e347410ff056a0206e7f
Adrian Macias <56698+iadrich@users.noreply.github.com>
2021-03-09 13:59:25 +0000

    Fixes to mysql

--- sayn/database/mysql.py
+++ sayn/database/mysql.py
@@ -1,6 +1,8 @@
-from sqlalchemy import create_engine
+import datetime
+import decimal
 
-# from sqlalchemy.types import Boolean, DateTime, Text
+from sqlalchemy import create_engine
+from sqlalchemy.sql import sqltypes
 
 from . import Database
 
@@ -9,7 +11,10 @@ db_parameters = ["host", "user", "password", "port", "database"]
 
 class Mysql(Database):
     def feature(self, feature):
-        return feature in ("CAN REPLACE VIEW",)
+        return feature in (
+            "CAN REPLACE VIEW",
+            "CANNOT SPECIFY DDL IN SELECT",
+        )
 
     def create_engine(self, settings):
         # Create engine using the connect_args argument to create_engine
@@ -40,3 +45,24 @@ class Mysql(Database):
             dst_table=dst_table,
             **ddl,
         )
+
+    def _py2sqa(self, from_type):
+        python_types = {
+            int: sqltypes.BigInteger,
+            str: sqltypes.Text,
+            float: sqltypes.Float,
+            decimal.Decimal: sqltypes.Numeric,
+            datetime.datetime: sqltypes.TIMESTAMP,
+            bytes: sqltypes.LargeBinary,
+            bool: sqltypes.Boolean,
+            datetime.date: sqltypes.Date,
+            datetime.time: sqltypes.Time,
+            datetime.timedelta: sqltypes.Interval,
+            list: sqltypes.ARRAY,
+            dict: sqltypes.JSON,
+        }
+
+        if from_type not in python_types:
+            raise ValueError(f'Type not supported "{from_type}"')
+        else:
+            return python_types[from_type]().compile(dialect=self.engine.dialect)
