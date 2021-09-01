7f1e469cf2961716670af4bd24e5175681051118
Adrian Macias <56698+iadrich@users.noreply.github.com>
2021-09-01 15:01:50 +0100

    Removed usage of importlib.metadata for python <3.8 compatibility

--- sayn/__init__.py
+++ sayn/__init__.py
@@ -1,9 +1,11 @@
-from importlib.metadata import version
+# from importlib.metadata import version
+#
+# try:
+#     __version__ = version(__name__)
+# except:
+#     pass
 
-try:
-    __version__ = version(__name__)
-except:
-    pass
+__version__ = "0.5.8"
 
 
 from .tasks import PythonTask
