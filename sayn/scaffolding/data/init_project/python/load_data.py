46fc2769c306adafe7080ca1b3c2513c806552d8
robin-173 <robin@173tech.com>
2022-02-10 18:03:34 +0000

    Replaced python tutorial file

--- sayn/scaffolding/data/init_project/python/load_data.py
+++ sayn/scaffolding/data/init_project/python/load_data.py
@@ -1,15 +1,15 @@
 import random
 from uuid import uuid4
-​
+
 from sayn import task
-​
-​
+
+
 @task(outputs=["logs_arenas", "logs_tournaments", "logs_battles", "logs_fighters"])
 def load_data(context, warehouse):
     fighters = ["Son Goku", "John", "Lucy", "Dr. x", "Carlos", "Dr. y?"]
     arenas = ["Earth Canyon", "World Edge", "Namek", "Volcanic Crater", "Underwater"]
     tournaments = ["World Championships", "Tenka-ichi Budokai", "King of the Mountain"]
-​
+
     context.set_run_steps(
         [
             "Generate Dimensions",
@@ -20,7 +20,7 @@ def load_data(context, warehouse):
             "Load battles",
         ]
     )
-​
+
     with context.step("Generate Dimensions"):
         # Add ids to the dimensions
         fighters = [
@@ -35,27 +35,27 @@ def load_data(context, warehouse):
             {"tournament_id": str(uuid4()), "tournament_name": val}
             for id, val in enumerate(tournaments)
         ]
-​
+
     with context.step("Generate Battles"):
         battles = list()
         for tournament in tournaments:
             tournament_id = tournament["tournament_id"]
             # Randomly select a number of battles to generate for each tournament
             n_battles = random.choice([10, 20, 30])
-​
+
             for _ in range(n_battles):
                 battle_id = str(uuid4())
-​
+
                 # Randomly choose fighters and arena
                 fighter1_id = random.choice(fighters)["fighter_id"]
                 fighter2_id = random.choice(
                     [f for f in fighters if f["fighter_id"] != fighter1_id]
                 )["fighter_id"]
                 arena_id = random.choice(arenas)["arena_id"]
-​
+
                 # Pick a winner
                 winner_id = fighter1_id if random.uniform(0, 1) <= 0.5 else fighter2_id
-​
+
                 battles.append(
                     {
                         "event_id": str(uuid4()),
@@ -67,15 +67,15 @@ def load_data(context, warehouse):
                         "winner_id": winner_id,
                     }
                 )
-​
+
     data_to_load = {
         "fighters": fighters,
         "arenas": arenas,
         "tournaments": tournaments,
         "battles": battles,
     }
-​
-    # Final logs load
+
+    # Load logs
     for log_type, log_data in data_to_load.items():
         with context.step(f"Load {log_type}"):
             warehouse.load_data(f"logs_{log_type}", log_data, replace=True)
