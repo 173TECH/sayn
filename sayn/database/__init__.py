f5be42602b98f7f59e083882af86d62ddac5dfff
Adrian Macias <56698+iadrich@users.noreply.github.com>
2023-05-18 19:41:28 +0100

    Fix load_data

--- sayn/database/__init__.py
+++ sayn/database/__init__.py
@@ -552,7 +552,7 @@ class Database:
             ddl = result.value
 
         check_create = True
-        table_exists_prior_load = self._object_exists(table, schema, db)
+        table_exists_prior_load = self._table_exists(table, schema)
 
         records_loaded = 0
         for i, record in enumerate(data):
