48ac84b76e20b3d9a7a74a19c36886ed2078497f
Sotiris Gkoulimaris <30499756+hustic@users.noreply.github.com>
2024-01-31 12:36:41 +0000

    Added Process class implementation exampe

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import concurrent.futures
 import time
 from collections import OrderedDict
 
-from scrap.test_process import Task
+from scrap.test_process import Task, TaskProcess
 
 from sayn.database.creator import create as create_db
 from sayn.utils.dag import topological_sort_step
@@ -11,6 +11,71 @@ from sayn.utils.dag import topological_sort_step
 
 N_TASK = 10
 N_CPU = 4
+
+
+def use_regular_class_and_executor(dag, connections):
+    """We can limit the number of processes that run concurently by changing the N_CPU
+    variable. So we can simulate sequential execution when needed (as default behaviour).
+    """
+    # Create tasks
+    tasks = dict()
+    for t in range(1, N_TASK + 1):
+        tasks[t] = Task()
+
+    print("Defined Tasks")
+
+    for i, t in tasks.items():
+        message = f"I am {i}"
+        t.setup(i, message, connections)
+
+    print("Setup Tasks")
+
+    # Get ids of tasks that can be run together
+    task_lists = list()
+    for task_list in topological_sort_step(dag):
+        task_lists.append(task_list)
+
+    with concurrent.futures.ProcessPoolExecutor(N_CPU) as executor:
+        for task_list in task_lists:
+            futures = {executor.submit(tasks[int(t)].run): t for t in task_list}
+
+            for future in concurrent.futures.as_completed(futures):
+                task_id = futures[future]
+                try:
+                    data = future.result()
+                except Exception as exc:
+                    print("%r generated an exception: %s" % (task_id, exc))
+                else:
+                    print("%r page is %d" % (task_id, data))
+
+    return
+
+
+def use_process_class(dag, connections):
+    """We can't limit the number of processes according to a set worker number.
+    I thought we would gain advantages in how objects are defined, but that is not the case.
+    """
+
+    # Create tasks
+    tasks = dict()
+
+    for t in range(1, N_TASK + 1):
+        message = f"I am {t}"
+        tasks[t] = TaskProcess(t, message, connections)
+
+    # Get ids of tasks that can be run together
+    task_lists = list()
+    for task_list in topological_sort_step(dag):
+        task_lists.append(task_list)
+
+    for task_list in task_lists:
+        for t in task_list:
+            tasks[int(t)].start()
+
+        for t in task_list:
+            tasks[int(t)].join()
+
+    return
 
 
 def main():
@@ -32,11 +97,6 @@ def main():
     # for connection in connections.values():
     #     connection._activate_connection()
 
-    # Create tasks
-    tasks = dict()
-    for t in range(1, N_TASK + 1):
-        tasks[t] = Task()
-
     # Define DAG based on tasks
     dag = {
         "1": [],
@@ -51,28 +111,9 @@ def main():
         "10": ["9"],
     }
 
-    print("Defined Tasks")
+    use_regular_class_and_executor(dag, connections)
 
-    for i, t in tasks.items():
-        message = f"I am {i}"
-        t.setup(i, message, connections)
-
-    print("Setup Tasks")
-
-    with concurrent.futures.ProcessPoolExecutor(N_CPU) as executor:
-        for task_list in topological_sort_step(dag):
-            # print(task_list)
-
-            futures = {executor.submit(tasks[int(t)].run): t for t in task_list}
-
-            for future in concurrent.futures.as_completed(futures):
-                task_id = futures[future]
-                try:
-                    data = future.result()
-                except Exception as exc:
-                    print("%r generated an exception: %s" % (task_id, exc))
-                else:
-                    print("%r page is %d bytes" % (task_id, data))
+    use_process_class(dag, connections)
 
 
 if __name__ == "__main__":
--- scrap/test_process.py
+++ scrap/test_process.py
@@ -1,5 +1,9 @@
+from multiprocessing import Process
+
 import time
 import random
+
+# Both implementations need to activate the connection in their Run method.
 
 
 class Task:
@@ -18,6 +22,38 @@ class Task:
         time.sleep(random.randint(2, 5))
         for c in self.connections.values():
             c._activate_connection()
+
+        result = self.connections["target_db"].read_data("SELECT 1")
+        print(result)
+        print(self.something)
+        time.sleep(1)
+        print("Done")
+        return 1
+
+
+class TaskProcess(Process):
+    def __init__(self, id, something, connections):
+        Process.__init__(self)
+        self.id = id
+        self.something = something
+        self.connections = connections
+
+        # I thought this could work here, but it doesn't
+        # for c in self.connections.values():
+        #     c._activate_connection()
+
+    def setup(self):
+        return
+
+    def run(self):
+        print("Starting work!")
+        time.sleep(random.randint(2, 5))
+
+        for c in self.connections.values():
+            c._activate_connection()
+
+        result = self.connections["target_db"].read_data("SELECT 1")
+        print(result)
         print(self.something)
         time.sleep(1)
         print("Done")
